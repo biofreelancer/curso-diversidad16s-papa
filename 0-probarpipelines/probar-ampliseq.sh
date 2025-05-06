51240006db1f31efd7bdcfd72cf89420df4a37b5
Israel Aguilar <iaguilaror@gmail.com>
2025-05-06 13:27:23 -0600

    add reports

--- 0-probarpipelines/probar-ampliseq.sh
+++ 0-probarpipelines/probar-ampliseq.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 nextflow run nf-core/ampliseq \
         -profile test,docker \
-        --outdir prueba-ampliseq \
-        --max_cpus 2 --max_memory 3.GB
+        --outdir resultados/prueba-ampliseq \
+        --max_cpus 2 --max_memory 3.GB \
+	-with-report resultados/`date +%Y%m%d_%H%M%S`_report.html \
+	-with-dag resultados/`date +%Y%m%d_%H%M%S`.DAG.html \
